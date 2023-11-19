5f8216b802792d1394f8fcef3a91a1534c579a02
Priyaranjan <priyaranjanmantri@gmail.com>
2023-11-19 16:06:00 +0530

    added: snap admin

--- src/main/java/com/tut/employee/EmployeeApplication.java
+++ src/main/java/com/tut/employee/EmployeeApplication.java
@@ -1,8 +1,11 @@
 package com.tut.employee;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tech.ailef.snapadmin.external.SnapAdminAutoConfiguration;
 
+@ImportAutoConfiguration(SnapAdminAutoConfiguration.class)
 @SpringBootApplication
 public class EmployeeApplication {
 
